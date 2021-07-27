adb1b6c3880709d4150461fff13912efcf748a15
gladiston1990 <63737797+gladiston1990@users.noreply.github.com>
2021-07-26 23:52:26 -0300

    Add files via upload

---
+++ tab1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main(){
+    int tab[10][10];
+    int x, y;
+
+    for(x=1;x<10;x++)
+    {
+        for(y=1;y<10;y++)
+        {
+        tab[x][y]=x*y;
+        }
+    }
+    for(x=1;x<10;x++)
+    {
+        printf("\n");
+
+    for(y=1;y<10;y++){
+
+    printf("%d x %d = %d\t",y,x,tab[x][y]);
+    }
+    }
+return (0);
+}
