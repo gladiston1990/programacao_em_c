6222d26119d58384c5242bdf9c22d6304e0e7b82
gladiston1990 <63737797+gladiston1990@users.noreply.github.com>
2021-07-27 00:01:08 -0300

    Add files via upload

--- conversao_de_valor_em_metros.c
+++ conversao_de_valor_em_metros.c
@@ -7,14 +7,14 @@ void main() {
     printf("Digite o valor em metros a ser convertido: ");
     scanf ("%f",&metros);
 
-    dec = metros / 10;
-    cen = metros / 100;
-    mili = metros / 1000;
+    dec = metros * 10;
+    cen = metros * 100;
+    mili = metros * 1000;
 
 
-    printf("\nO valor em metros convertido em decimentos e = %f\n", dec);
-    printf("\nO valor em metros convertido em centimentos e = %f\n", cen);
-    printf("\nO valor em metros convertido em milimentos e = %f\n", mili);
+    printf("\nO valor em metros convertido em decimentos e = %2.f\n", dec);
+    printf("\nO valor em metros convertido em centimentos e = %2.f\n", cen);
+    printf("\nO valor em metros convertido em milimentos e = %2.f\n", mili);
 
 
 }
