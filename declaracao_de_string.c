e129efd222bc4433b5d0a87468773aef06018b82
gladiston1990 <63737797+gladiston1990@users.noreply.github.com>
2021-07-28 19:18:21 -0300

    Add files via upload

---
+++ declaracao_de_string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[] = "hello word";
+
+    printf("hellohellohellohellohello %s", str);
+
+    char str = "hellohellohellohellohello";
+
+    prinf("%s", str);
+
+    return(0);
+
+}
